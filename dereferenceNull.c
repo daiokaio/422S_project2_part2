40af14fcdaefd8402e3af239cec70b32b8d13f24
daiokaio <daiokaio@gmail.com>
2016-10-16 19:52:22 -0500

    update dereferenceNull.c
    make it trap

--- dereferenceNull.c
+++ dereferenceNull.c
@@ -7,16 +7,10 @@ main(int argc, char *argv[])
 {
   printf(1,"Beginning Dereference Function \n");
 
-  //This way seems to work
-  char *a;
-  *a = 0;
-  a[0] = 'a';
-
   //vs doing it this way... This way makes it trap
-  // char *a = 0;
-  // a[0] = 'a';
-
-
+  char *a = 0;
+  a[0] = 'a';
+  
   printf(1,"Finished dereferencing \n\n");
   printf(1, "Value of deference a: %x", &a);
   exit();
